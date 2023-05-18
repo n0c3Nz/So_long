442cf28c5d0a1ebd0fc6ad88c226abb417740b48
4g4x0r <guortun-@c1r9s4.42madrid.com>
2023-05-18 21:39:33 +0200

    Ultima del campus por hoy

--- estructura.c
+++ estructura.c
@@ -10,6 +10,7 @@ int	main(int argc, char **argv)
 	c.mlx_win = mlx_new_window(c.mlx, c.columns * BPP, c.lines * BPP, "Juego");
 	mlx_process(&c);
 	mlx_key_hook(c.mlx_win, key_hook, &c);
+	system("leaks juego");
 	mlx_loop(c.mlx);
 	return(0);
 }
